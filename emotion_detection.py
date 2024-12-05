5c837a04c5ed972b25ae2d33692387718265378c
goe86 <georgegamil1986@hotmail.com>
2024-12-05 05:15:34 -0500

    added the emotions as output, with maximum emotion mentioned

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,7 +1,22 @@
 import requests
+import json
+
 def emotion_detector(text_to_analyze):
     URL= 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    
     Headers= {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    
     Input_json={ "raw_document": { "text": text_to_analyze }}
+    
     response= requests.post(url=URL, headers=Headers, json=Input_json)
-    return response.json()
+
+    json_response=response.json()
+    emotions=json_response["emotionPredictions"][0]
+    all_emotions={}
+
+    for key, value in emotions['emotion'].items():
+        all_emotions[key]= value
+    
+    dominant_emotion=max(all_emotions.items(),key=lambda x: x[1])
+    all_emotions['dominant_emotion']= dominant_emotion[0] 
+    return all_emotions
